01fb28db488e912316a8c267d417b15b9ed67cc3
Steven Yuan <smartguru478@gmail.com>
2023-06-12 21:39:42 -0700

    wowcards

--- src/callback/callback.controller.ts
+++ src/callback/callback.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query , Body, Post, HttpStatus, HttpException} from '@nestjs/common';
+import { now, reject } from 'lodash';
+import { resolve } from 'path';
 
 @Controller('callback')
 export class CallbackController {
     constructor(
     ){}
 
-    @Get()
-    handleCallback(){
-        console.log("Go there!!!")
-        return {message: 'Callback received successfully'};
+    @Post()
+    handleCallback(@Body() data: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            try {
+                console.log(data)
+
+                resolve({ status: "ok"})
+            } catch (error) {
+                reject({status: 'Error', message: 'Error processing callback'})
+            }
+        })
     }
 }
